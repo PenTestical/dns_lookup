bbfe13ad15e8fedb0f265778f40f8bd2a3e67f74
PenTestical <57206134+PenTestical@users.noreply.github.com>
2020-12-17 01:17:46 +0100

    Update get_hostname.sh

--- get_hostname.sh
+++ get_hostname.sh
@@ -3,4 +3,4 @@
 #bash one liner to get hostnames within a domain, reverse the ips and cut off unnecessary output
 #it creates a new text file "hostnames.txt" which contains the hostnames belonging to a particular IP address
 
-for ip in {0..255}; do nslookup 10.11.1.$ip 10.11.1.220 | grep -v "NXDOMAIN" | grep . | cut -f1,3 -d " ";  ip=+1; done | awk '{split($1,p,"."); print p[4] "." p[3] "." p[2] "." p[1], $2, $3}' | for ip in {0..255}; do nslookup 10.11.1.$ip 10.11.1.220 | grep -v "NXDOMAIN" | grep . | cut -f1,3 -d " ";  ip=+1; done | awk '{split($1,p,"."); print p[4] "." p[3] "." p[2] "." p[1], $2, $3}' | sed 's/.$//' > hostnames.txt
+for ip in {0..255}; do nslookup 10.11.1.$ip 10.11.1.220 | grep -v "NXDOMAIN" | grep . | cut -f1,3 -d " ";  ip=+1; done | awk '{split($1,p,"."); print p[4] "." p[3] "." p[2] "." p[1], $2, $3}' | sed 's/.$//'
